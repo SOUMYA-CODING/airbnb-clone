11b07d683b0ac8100c3a82a0d070d4a677611027
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-09-20 16:15:11 +0530

    confirm

--- backend/hotels/models.py
+++ backend/hotels/models.py
@@ -12,6 +12,8 @@ class Country(models.Model):
 # Category
 class Category(models.Model):
     name = models.CharField(max_length=25, null=True, blank=True)
+    photo = models.ImageField(
+        upload_to="categoryImages/", null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -21,7 +23,8 @@ class Category(models.Model):
 class Hotels(models.Model):
     placename = models.CharField(max_length=70)
     country = models.ForeignKey(Country, on_delete=models.DO_NOTHING)
-    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, blank=True, null=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.DO_NOTHING, blank=True, null=True)
     hotelname = models.CharField(max_length=70)
     description = models.TextField()
     cost = models.FloatField()
